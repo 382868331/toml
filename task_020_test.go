9ec6d265857b63f49608c0c58ed13fe7078b25e4
382868331 <382868331@users.noreply.github.com>
2026-08-24 02:51:21 +0800

    challenge: reproduce 数组编码产生尾逗号

---
+++ task_020_test.go
@@ -0,0 +1,16 @@
+package toml
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestTask020ArrayNoTrailingComma(t *testing.T) {
+	b, e := Marshal(struct{ Values []int }{[]int{1, 2}})
+	if e != nil {
+		t.Fatal(e)
+	}
+	if strings.Contains(string(b), "2, ]") {
+		t.Fatalf("trailing comma: %s", b)
+	}
+}
