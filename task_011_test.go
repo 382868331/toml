7f07fb3f6a0bd9523b414d7115ee82c3f108e967
382868331 <382868331@users.noreply.github.com>
2026-08-24 02:37:33 +0800

    challenge: reproduce 布尔值反转

---
+++ task_011_test.go
@@ -0,0 +1,13 @@
+package toml
+
+import "testing"
+
+func TestTask011TrueBoolean(t *testing.T) {
+	var v struct{ Enabled bool }
+	if _, e := Decode("Enabled = true", &v); e != nil {
+		t.Fatal(e)
+	}
+	if !v.Enabled {
+		t.Fatal("Enabled=false, want true")
+	}
+}
